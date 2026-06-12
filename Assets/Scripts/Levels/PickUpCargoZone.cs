c089037fecbc5cb3cc89a20f5af5983c3c7642bb
unknown <vova.vevo666@gmail.com>
2026-06-12 17:41:12 +0300

    add sprites

--- Assets/Scripts/Levels/PickUpCargoZone.cs
+++ Assets/Scripts/Levels/PickUpCargoZone.cs
@@ -24,6 +24,7 @@ namespace Assets.Scripts.Levels
                 {
                     isFinishSendAction = true;
                     onFinished?.Invoke();
+                    Destroy(gameObject);
                 }
 
             }
